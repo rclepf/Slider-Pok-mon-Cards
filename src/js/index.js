8c453a9137b6819781b3c05cf0a1fd8215e4ff06
rclepf <rclepf@gmail.com>
2022-08-06 09:53:49 -0300

    Ajuste Arrow

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,31 @@
-const btnAvancar = document.getElementById("btn-avancar");
-const btnVoltar = document.getElementById("btn-voltar");
+const btnAvancar = document.getElementsById("btn-avancar");
+const btnVoltar = document.getElementsById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
-function esconderCartaoSelecionado(){
-    const cartaoSelecionado = document.querySelector(".selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
 }
 
-function mostrarCartao(indiceCartao){
-    cartoes[indiceCartao].classList.add("selecionado");
+function mostrarCartao(indiceCartao) {
+  cartoes[indiceCartao].classList.add("selecionado");
 }
 
-btnAvancar.addEventListener("click", function() {
-    if(cartaoAtual === cartoes.length - 1) return;
+btnAvancar.addEventListener("click", function () {
+  if (cartaoAtual === cartoes.length - 1) return;
 
-    esconderCartaoSelecionado();
-   
-    cartaoAtual++;
-    mostrarCartao(cartaoAtual);
+  esconderCartaoSelecionado();
+
+  cartaoAtual++;
+  mostrarCartao(cartaoAtual);
 });
 
-btnVoltar.addEventListener("click", function() {
-    if(cartaoAtual === 0) return;
+btnVoltar.addEventListener("click", function () {
+  if (cartaoAtual === 0) return;
 
-    esconderCartaoSelecionado();
-   
-    cartaoAtual--;
-    mostrarCartao(cartaoAtual);
-    
+  esconderCartaoSelecionado();
+
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual);
 });
